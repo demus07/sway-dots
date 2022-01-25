4f5c3e0f12b1a99e85c1556351fc606bc0fa29b4
demus <84559645+demus07@users.noreply.github.com>
2022-01-25 09:40:21 +0530

    Add files via upload

---
+++ configuration.nix
@@ -0,0 +1,135 @@
+# Edit this configuration file to define what should be installed on
+# your system.  Help is available in the configuration.nix(5) man page
+# and in the NixOS manual (accessible by running ‘nixos-help’).
+
+{ config, pkgs, ... }:
+
+{
+  imports =
+    [ # Include the results of the hardware scan.
+      ./hardware-configuration.nix
+    ];
+
+	 nixpkgs.config.allowUnfree = true;  #Sorry stallman sir
+
+  nix = {
+	 autoOptimiseStore = true;
+	package = pkgs.nixFlakes;
+	extraOptions = ''
+		experimental-features = nix-command flakes
+	'';
+   };
+
+  # Fonts
+  fonts.fonts = with pkgs; [
+  (nerdfonts.override { fonts = [ "Mononoki" "JetBrainsMono" ]; })
+];
+
+  # Networking
+   networking.networkmanager.enable = true;
+
+  # Use the systemd-boot EFI boot loader.
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
+
+   networking.hostName = "nix"; # Define your hostname.
+
+  # Set your time zone.
+   time.timeZone = "Asia/Kolkata";
+
+  # Select internationalisation properties.
+   i18n.defaultLocale = "en_US.UTF-8";
+   console = {
+     font = "Lat2-Terminus16";
+     keyMap = "us";
+   };
+
+  # Enable sound.
+   sound.enable = true;
+   hardware.pulseaudio.enable = true;
+
+  # Enable touchpad support (enabled default in most desktopManager).
+   services.xserver.libinput.enable = true;
+
+  # Define a user account. Don't forget to set a password with ‘passwd’.
+   users.users.demus= {
+     isNormalUser = true;
+     extraGroups = [ "wheel" "networkmanager" "users" ]; 
+   };
+
+   boot.kernelPackages = pkgs.linuxPackages_latest;
+
+
+
+   programs.sway = {
+   	enable = true;
+	wrapperFeatures.gtk = true;
+	extraPackages = with pkgs; [
+	swaylock-fancy
+	swaylock-effects
+	dmenu
+	waybar
+	alacritty
+	];
+   };
+
+  # List packages installed in system profile. To search, run:
+  # $ nix search wget
+   environment.systemPackages = with pkgs; [
+        vscode
+        neovim 
+        wget
+	usbutils
+        coreutils
+        git
+     	gcc
+	ripgrep
+	exa
+	bat
+	tree
+	fd
+	kitty
+	starship
+	pfetch
+	brave
+	discord
+	discocss
+	zathura
+	grim
+	slurp
+	mako
+	lxappearance
+	papirus-icon-theme
+	brightnessctl
+  		
+   ];
+
+  # Some programs need SUID wrappers, can be configured further or are
+  # started in user sessions.
+  # programs.mtr.enable = true;
+  # programs.gnupg.agent = {
+  #   enable = true;
+  #   enableSSHSupport = true;
+  # };
+
+  # List services that you want to enable:
+
+  # Enable the OpenSSH daemon.
+  # services.openssh.enable = true;
+
+  # Open ports in the firewall.
+  # networking.firewall.allowedTCPPorts = [ ... ];
+  # networking.firewall.allowedUDPPorts = [ ... ];
+  # Or disable the firewall altogether.
+  # networking.firewall.enable = false;
+
+  # This value determines the NixOS release from which the default
+  # settings for stateful data, like file locations and database versions
+  # on your system were taken. It‘s perfectly fine and recommended to leave
+  # this value at the release version of the first install of this system.
+  # Before changing this value read the documentation for this option
+  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
+  system.stateVersion = "22.05"; # Did you read the comment?
+
+}
+
